255d70b7c89eb6cfd9dd4f46db91c6cdaf3bb6b2
ahmetgsu <ahmet.gsu@gmail.com>
2019-09-25 22:54:47 +0300

    15-install mongoose & connect

--- node-express-mongo-dev-to-deployment/vidjot/app.js
+++ node-express-mongo-dev-to-deployment/vidjot/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// Connect to mongoose
+mongoose
+  .connect("mongodb://localhost/vidjot-dev", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
 
 //Handlebars middleware
 app.engine("handlebars", exphbs());
