953107c806ce89e7fde130c350b8cf2cd0e5186c
ahmetgsu <ahmet.gsu@gmail.com>
2019-09-27 18:51:11 +0300

    28-register form validation

--- node-express-mongo-dev-to-deployment/vidjot/routes/users.js
+++ node-express-mongo-dev-to-deployment/vidjot/routes/users.js
@@ -12,4 +12,31 @@ router.get('/register', (req, res) => {
   res.render('users/register');
 });
 
+// Register Form POST
+router.post('/register', (req, res) => {
+  //Password validation
+  let errors = [];
+
+  if (req.body.password != req.body.password2) {
+    errors.push({ text: 'Passwords do not match' });
+  }
+
+  if (req.body.password.length < 4) {
+    errors.push({ text: 'Password must be at least 4 characters' });
+  }
+  // if errors has sth in it re-render the form
+  if (errors.length > 0) {
+    // params means re-render the form with these params
+    res.render('users/register', {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    });
+  } else {
+    res.send('passed');
+  }
+});
+
 module.exports = router;
