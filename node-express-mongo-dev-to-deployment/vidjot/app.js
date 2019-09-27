56f091749d95c99821c4987d6764b890a9e67429
ahmetgsu <ahmet.gsu@gmail.com>
2019-09-27 23:35:23 +0300

    31-finishing the login functionality

--- node-express-mongo-dev-to-deployment/vidjot/app.js
+++ node-express-mongo-dev-to-deployment/vidjot/app.js
@@ -55,6 +55,12 @@ app.use(
   })
 );
 
+// Passport middleware
+// It's very important to place this after the Exp Session middleware
+// otherwise, we'll have some issues
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Connect flash middleware
 app.use(flash());
 
@@ -63,6 +69,7 @@ app.use(function(req, res, next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
   next();
 });
 
--- node-express-mongo-dev-to-deployment/vidjot/config/passport.js
+++ node-express-mongo-dev-to-deployment/vidjot/config/passport.js
@@ -8,7 +8,37 @@ const User = mongoose.model('users');
 module.exports = function(passport) {
   passport.use(
     new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-      console.log(email);
+      // Login Page User check
+      User.findOne({
+        email: email
+      }).then(user => {
+        // User does not exist
+        if (!user) {
+          // done 1st arg is error. 2nd is user. 3rd error msg
+          return done(null, false, { message: 'No User Found' });
+        }
+        // If user exist, Match password
+        // password is before encryption, user.password is after encryption
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) throw err;
+          if (isMatch) {
+            // it means, no err, everything is matched
+            return done(null, user);
+          } else {
+            return done(null, false, { message: 'Password Incorrect' });
+          }
+        });
+      });
     })
   );
+
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
 };
