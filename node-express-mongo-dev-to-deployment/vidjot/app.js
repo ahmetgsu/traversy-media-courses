9d83a15711d3bfe240823447aafecda3576f7e52
ahmetgsu <ahmet.gsu@gmail.com>
2019-09-27 14:23:44 +0300

    27-login and register pages

--- node-express-mongo-dev-to-deployment/vidjot/app.js
+++ node-express-mongo-dev-to-deployment/vidjot/app.js
@@ -1,46 +1,51 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-const exphbs = require("express-handlebars");
-const flash = require("connect-flash");
-const session = require("express-session");
-const mongoose = require("mongoose");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const exphbs = require('express-handlebars');
+const flash = require('connect-flash');
+const session = require('express-session');
+const mongoose = require('mongoose');
 
 const app = express();
 
 // Load Routes
-const ideas = require("./routes/ideas");
-const users = require("./routes/users");
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
 
 // Connect to mongoose
 mongoose
-  .connect("mongodb://localhost/vidjot-dev", {
+  .connect('mongodb://localhost/vidjot-dev', {
     useUnifiedTopology: true,
     useNewUrlParser: true
   })
-  .then(() => console.log("MongoDB Connected..."))
+  .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
 //Handlebars middleware
 app.engine(
-  "handlebars",
+  'handlebars',
   exphbs({
-    defaultLayout: "main"
+    defaultLayout: 'main'
   })
 );
-app.set("view engine", "handlebars");
+app.set('view engine', 'handlebars');
+
+// Static folder
+// it sets the public folder to be the express static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // BodyParser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Method-Override middleware
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 
 // Express Session middleware
 app.use(
   session({
-    secret: "secret",
+    secret: 'secret',
     resave: true,
     saveUninitialized: true
   })
@@ -51,28 +56,28 @@ app.use(flash());
 
 // Global variables for messages
 app.use(function(req, res, next) {
-  res.locals.success_msg = req.flash("success_msg");
-  res.locals.error_msg = req.flash("error_msg");
-  res.locals.error = req.flash("error");
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
   next();
 });
 
 //Index Route
-app.get("/", (req, res) => {
-  const title = "Welcome";
-  res.render("index", {
+app.get('/', (req, res) => {
+  const title = 'Welcome';
+  res.render('index', {
     title: title
   });
 });
 
 //About Route
-app.get("/about", (req, res) => {
-  res.render("about");
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
 // Use routes
-app.use("/ideas", ideas);
-app.use("/users", users);
+app.use('/ideas', ideas);
+app.use('/users', users);
 
 const port = 5000;
 
