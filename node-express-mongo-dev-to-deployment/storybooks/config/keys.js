73e326fb0770fde563fc774050eba686b48b8a94
ahmetgsu <ahmet.gsu@gmail.com>
2019-09-30 12:16:47 +0300

    40-creating our OAuth credentials

---
+++ node-express-mongo-dev-to-deployment/storybooks/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://ahmetkenar:12345@cluster0-54gm9.mongodb.net/test?retryWrites=true&w=majority',
+  googleClientID:
+    '964653328468-l9ld6neq29k30jn9ksoqp3b1u6ms3s8e.apps.googleusercontent.com',
+  googleClientSecret: 'BX7-375MH5_m_lIjo5Tcy-4V'
+};
