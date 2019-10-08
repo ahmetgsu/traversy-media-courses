fb8fe30c3208a593b79d5de0daf75f1448336117
ahmetgsu <ahmet.gsu@gmail.com>
2019-10-08 16:27:42 +0300

    57-edit & update contact action

--- react-front-to-back-2019/contact-keeper/client/src/context/contact/ContactState.js
+++ react-front-to-back-2019/contact-keeper/client/src/context/contact/ContactState.js
@@ -65,7 +65,11 @@ const ContactState = props => {
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
   };
+
   // Update Contact
+  const updateContact = contact => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
 
   // Filter Contacts
 
@@ -79,7 +83,8 @@ const ContactState = props => {
         addContact,
         deleteContact,
         setCurrent,
-        clearCurrent
+        clearCurrent,
+        updateContact
       }}
     >
       {props.children}
